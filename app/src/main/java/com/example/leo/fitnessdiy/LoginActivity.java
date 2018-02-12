d0ebe516699dff9e22c034eff5af6ef8738548bc
rizkihalasan <13515095@std.stei.itb.ac.id>
2018-02-12 15:28:59 +0700

    halaman login

--- app/src/main/java/com/example/leo/fitnessdiy/LoginActivity.java
+++ app/src/main/java/com/example/leo/fitnessdiy/LoginActivity.java
@@ -1,14 +1,18 @@
 package com.example.leo.fitnessdiy;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void doLogin(View view) {
+
     }
 }
