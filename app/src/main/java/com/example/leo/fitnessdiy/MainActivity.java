6fc98590aca371bc9c7a22d99ebb3f9f6084a316
unknown <leo112071@gmail.com>
2018-02-12 16:58:05 +0700

    Fix route

--- app/src/main/java/com/example/leo/fitnessdiy/MainActivity.java
+++ app/src/main/java/com/example/leo/fitnessdiy/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -12,6 +13,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration);
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView(R.layout.activity_main);
+
+        Thread welcomeThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(10000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+
+        welcomeThread.start();
     }
 }
