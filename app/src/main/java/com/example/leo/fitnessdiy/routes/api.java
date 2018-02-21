91f18db490c4853d0fb21143734d0f1acdd80c38
unknown <leo112071@gmail.com>
2018-02-21 19:27:32 +0700

    Several changes

--- app/src/main/java/com/example/leo/fitnessdiy/routes/api.java
+++ app/src/main/java/com/example/leo/fitnessdiy/routes/api.java
@@ -5,7 +5,7 @@ package com.example.leo.fitnessdiy.routes;
  */
 
 public class api {
-    public static final String BASE_URL = "http://192.168.100.102/fitness_server/";
+    public static final String BASE_URL = "http://10.5.26.200/fitness-server/";
 
     public static final String HISTORY_URL = BASE_URL + "history.php?user=";
 }
